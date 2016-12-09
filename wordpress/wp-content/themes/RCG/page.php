8b904872710e7effb17908c1ec6c938663228f7b
Tom Offringa <tom.offringa@wearespindle.com>
2016-12-09 16:01:33 +0100

    Added single page with info for gameverslagen.

--- wordpress/wp-content/themes/RCG/page.php
+++ wordpress/wp-content/themes/RCG/page.php
@@ -8,15 +8,4 @@
     </div>
 </div>
 
-<div class="l-container is-full-width">
-    <div class="l-full sponsorships">
-        <h3>Sponsors</h3>
-
-        <ul class="sponsorships-list">
-            <li><img src="/images/spindle.png" alt="Devhouse Spindle"/></li>
-            <li><img src="/images/wysz_fysio.png" alt="WYSZ Fysiotherapie" /></li>
-        </ul>
-    </div>
-</div>
-
 <?php get_footer(); ?>
--- wordpress/wp-content/themes/RCG/single.php
+++ wordpress/wp-content/themes/RCG/single.php
@@ -1,4 +1,30 @@
 <?php get_header(); ?>
 <?php the_post(); ?>
 
+<div class="l-container is-full-width">
+    <div class="l-full content">
+        <h1><?php the_title(); ?></h1>
+
+        <div class="l-two-third content">
+            <?php the_content(); ?>
+        </div>
+
+
+        <div class="scoreboard single">
+            <div class="team">
+                <div class="team-name"><?php the_field('hometeam_name'); ?></div>
+                <div class="team-score home-team">
+                    <?php the_field('hometeam_score'); ?>
+                </div>
+            </div><div class="team">
+                <div class="team-name"><?php the_field('outteam_name'); ?></div>
+                <div class="team-score">
+                    <?php the_field('outteam_score'); ?>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
 <?php get_footer(); ?>
