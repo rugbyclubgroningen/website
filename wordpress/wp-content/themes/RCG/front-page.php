6711a40b78d3cf2e6e68498d6a9de93b0ee8d5b8
Tom Offringa <tom.offringa@wearespindle.com>
2016-12-09 14:40:20 +0100

    Added WP to project

---
+++ wordpress/wp-content/themes/RCG/front-page.php
@@ -0,0 +1,139 @@
+<?php get_header(); ?>
+
+<div class="l-container is-full-width">
+    <div class="l-full sponsorships">
+        <h3>Sponsors</h3>
+
+        <ul class="sponsorships-list">
+            <li><img src="images/spindle.png" alt="Devhouse Spindle"/></li>
+            <li><img src="images/wysz_fysio.png" alt="WYSZ Fysiotherapie" /></li>
+        </ul>
+    </div>
+</div>
+
+<div class="l-container is-full-width">
+    <div class="l-full news-container">
+        <div class="news-container-two-third">
+
+            <div class="news-block">
+                <h3>Nieuws</h3>
+                <span class="news-date">13 FEB 2016</span>
+                <div class="news-block-content">
+                    <h4>Wedstrijdverslag</h4>
+                    <div class="scoreboard">
+                        <div class="team">
+                            <div class="team-name">RC Eemland 1</div>
+                            <div class="team-score home-team">
+                                9
+                            </div>
+                        </div><div class="team">
+                            <div class="team-name">RC Groningen 1</div>
+                            <div class="team-score">
+                                72
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="news-block">
+                <div class="news-block-bg" style="background-image: url('images/newsbg.png'); "></div>
+                <span class="news-date">13 FEB 2016</span>
+                <div class="news-block-content">
+                    <h4>Wedstrijdverslag</h4>
+                    <div class="scoreboard">
+                        <div class="team">
+                            <div class="team-name">RC Groningen 1</div>
+                            <div class="team-score home-team">
+                                16
+                            </div>
+                        </div><div class="team">
+                            <div class="team-name">RC Delft 1</div>
+                            <div class="team-score">
+                                3
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="news-block">
+                <span class="news-date">13 FEB 2016</span>
+                <div class="news-block-content"><h4>Nieuwe website</h4></div>
+            </div>
+
+            <div class="news-block">
+                <span class="news-date">13 FEB 2016</span>
+                <div class="news-block-content"><h4>Wedstrijd verslag</h4></div>
+            </div>
+
+            <div class="news-block filler"></div>
+
+            <div class="news-block filler"></div>
+        </div>
+
+        <div class="agenda-container">
+            <h3>Agenda</h3>
+
+            <div class="agenda-list">
+                <h4>Saturday 3 dec 2016</h4>
+                <ul>
+                    <li>
+                        <span class="agenda-time">11:00 AM</span>
+                        <span class="agenda-item">RC Groningen 1 - Club Friesland 1</span>
+                    </li>
+                    <li>
+                        <span class="agenda-time">12:00 PM</span>
+                        <span class="agenda-item">Junioren Gelderland - Twente 2</span>
+                    </li>
+                    <li>
+                        <span class="agenda-time">14:00 PM</span>
+                        <span class="agenda-item">Spindle - RC Groningen 1</span>
+                    </li>
+                    <li>
+                        <span class="agenda-time">14:00 AM</span>
+                        <span class="agenda-item">RC Groningen 2 - Club Friesland 2</span>
+                    </li>
+                </ul>
+
+                <h4>Saturday 10 dec 2016</h4>
+                <ul>
+                    <li>
+                        <span class="agenda-time">11:00 AM</span>
+                        <span class="agenda-item">RC Groningen 1 - Club Friesland 1</span>
+                    </li>
+                    <li>
+                        <span class="agenda-time">12:00 PM</span>
+                        <span class="agenda-item">Junioren Gelderland - Twente 2</span>
+                    </li>
+                    <li>
+                        <span class="agenda-time">14:00 PM</span>
+                        <span class="agenda-item">Spindle - RC Groningen 1</span>
+                    </li>
+                    <li>
+                        <span class="agenda-time">14:00 AM</span>
+                        <span class="agenda-item">RC Groningen 2 - Club Friesland 2</span>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="agenda-list-events">
+                <h3>Agenda</h3>
+
+                <div class="agenda-list">
+                    <h4>Tuesday 5 dec 2016</h4>
+                    <ul>
+                        <li>
+                            <span class="agenda-time">19:00 PM</span>
+                            <span class="agenda-item">Sinterklaas celebrations</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<?php get_footer(); ?>
---
+++ wordpress/wp-content/themes/RCG/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>
+<html lang="nl">
+
+<head>
+    <meta charset="utf-8">
+    <!--[if lt IE 9]><meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible"><![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="apple-mobile-web-app-title" content="">
+    <meta name="theme-color" content="#d82e3a">
+    <meta name="msapplication-TileImage" content="favicon-144.png">
+    <meta name="msapplication-TileColor" content="#d82e3a">
+
+    <title>Rugby Club Groningen</title>
+    <meta name="description" content="De tofste rugby club van Groningen!">
+    <meta name="author" content="RCG">
+
+    <link rel="stylesheet" href="/stylesheets/styles.css">
+    <link rel="shortcut icon" href="favicon-32.ico">
+    <link rel="apple-touch-icon-precomposed" href="favicon-152.png">
+</head>
+<body>
+
+    <div class="l-wrapper header" style="background-image: url('images/header.png');">
+        <div class="l-full">
+            <div class="logo">
+                <img src="images/logo.svg" alt="Rugby Club Groningen"/>
+            </div>
+
+             <a href="#" class="navigation-toggle">
+                 <span class="navigation-desc">MENU</span>
+                 <div class="navigation-symbol"><span></span><span></span><span></span></div>
+             </a>
+
+            <ul class="navigation">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Nieuws</a></li>
+                <li><a href="#">Over ons</a></li>
+                <li><a href="#">Teams</a></li>
+                <li><a href="#">Sponsoren</a></li>
+                <li class="button nav-cta"><a href="#">Lid worden</a></li>
+            </ul>
+       </div>
+
+       <div class="l-full intro-cta">
+           <h2>"Rugby is great. The players don't wear helmets or padding; they just beat the living daylights out of each other and then go for a beer. I love that."</h2>
+       </div>
+
+       <div class="l-full u-text-center">
+           <a href="#" class="button is-cta">Deelnemen aan introductie training</a>
+       </div>
+
+   </div>
